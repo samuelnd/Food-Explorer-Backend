b247a15ac3e2b094487875fa1a9556c7a0613475
Samuel Nunes <108309380+samuelnd@users.noreply.github.com>
2023-05-19 16:44:04 -0300

    Delete Back_end_Explorer_Food-main directory

--- Back_end_Explorer_Food-main/src/database/knex/migrations/20221113173008_alterFavorite.js
+++
@@ -1,8 +0,0 @@
-exports.up = knex =>
-  knex.schema.alterTable('favorites', table => {
-    table.integer('favoriteList')
-
-    table.dropColumn('plate_id')
-  })
-
-exports.down = knex => knex.schema.dropTable('favorites')
--- Back_end_Explorer_Food-main/src/database/knex/migrations/20221113183509_alterFavorites.js
+++
@@ -1,6 +0,0 @@
-exports.up = knex =>
-  knex.schema.alterTable('favorites', table => {
-    table.dropColumn('favoriteList')
-  })
-
-exports.down = knex => knex.schema.dropTable('favorites')
